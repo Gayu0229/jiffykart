0ed65da114236c11ba903a90cd915761cbfed231
jiffykart <jiffykart@gmai.com>
2026-08-29 17:20:30 +0530

    Backup before updating

--- Frontend/jiffy-kart/hooks/index.ts
+++
@@ -1,6 +0,0 @@
-
-export { useAuth } from '../context/AuthContext';
-export { useCart } from '../context/CartContext';
-export { useNavigation } from '../context/NavigationContext';
-export { useFavorites } from '../context/FavoritesContext';
-export { useComparison } from '../context/ComparisonContext';
